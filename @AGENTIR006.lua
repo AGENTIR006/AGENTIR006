e771215b4c6ecb8c397f9462642807dffbf9a595
AGENTIR006 <148233435+AGENTIR006@users.noreply.github.com>
2023-11-02 19:27:24 +0300

    Update @AGENTIR006.lua

--- @AGENTIR006.lua
+++ @AGENTIR006.lua
@@ -8,3 +8,53 @@ label:UpdateLabel("New Text")
 Section:NewButton("ButtonText", "ButtonInfo", function()
     print("Clicked")
 end)
+button:UpdateButton("New Text")
+
+
+
+
+
+
+
+
+
+
+
+Section:NewKeybind("KeybindText", "KeybindInfo", Enum.KeyCode.F, function()
+	Library:ToggleUI()
+end)
+
+local oldList = {
+  "2019",
+  "2020"
+}
+local newList = {
+  "2021",
+  "2022"
+}
+local dropdown = Section:NewDropdown("Dropdown","Info", oldList, function()
+
+end)
+Section:NewButton("Update Dropdown", "Refreshes Dropdown", function()
+  dropdown:Refresh(newList)
+end)
+
+Section:NewColorPicker("Color Text", "Color Info", Color3.fromRGB(0,0,0), function(color)
+    print(color)
+end)
+
+local colors = {
+    SchemeColor = Color3.fromRGB(0,255,255),
+    Background = Color3.fromRGB(0, 0, 0),
+    Header = Color3.fromRGB(0, 0, 0),
+    TextColor = Color3.fromRGB(255,255,255),
+    ElementColor = Color3.fromRGB(20, 20, 20)
+}
+
+local Window = Library.CreateLib("TITLE", colors)
+
+for theme, color in pairs(themes) do
+    Section:NewColorPicker(theme, "Change your "..theme, color, function(color3)
+        Library:ChangeColor(theme, color3)
+    end)
+end
