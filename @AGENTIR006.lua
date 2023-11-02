7e2093df60d9ff92d8840e407f7896fcdd0f2d47
AGENTIR006 <148233435+AGENTIR006@users.noreply.github.com>
2023-11-02 14:33:25 +0300

    Update @AGENTIR006.lua

--- @AGENTIR006.lua
+++ @AGENTIR006.lua
@@ -1,3 +1,4 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/slf0Dev/Goat-poop/main/Windows%2010%20UI%20Library"))()
-local MainWindow = Library.Main("GuiName","KeyBind")
-local Category = MainWindow.Category("Your Text","ImageId","ImageScaleType",Image Transparency)
+local XanaxUILib = loadstring(game:HttpGet("https://pastebin.com/raw/XZz3Ytbu"))()
+local Ui = XanaxUILib:CreateWindow("Example UI")
+local Info = Ui:CreateSection("Info")
+local ExampleSection = Ui:CreateSection("Example")
